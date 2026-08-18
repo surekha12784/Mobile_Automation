6aa4f021e9a78f261583352012695df30cb3b35a
SMADA25 <Surekha.Madala@albertsons.com>
2026-08-18 20:16:25 +0530

    addbasetest

--- src/main/java/org/rahulshettyacademy/TestUtils/BaseTest.java
+++ src/main/java/org/rahulshettyacademy/TestUtils/BaseTest.java
@@ -2,72 +2,71 @@ package org.rahulshettyacademy.TestUtils;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
-import io.appium.java_client.service.local.AppiumDriverLocalService;
-import io.appium.java_client.service.local.AppiumServiceBuilder;
 import org.rahulshettyacademy.pageObjects.android.FormPage;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
-import java.io.*;
-import java.net.MalformedURLException;
-    import java.net.URL;
-    import java.time.Duration;
-    import java.util.Properties;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.time.Duration;
+import java.util.Properties;
 
-    public class BaseTest {
-        public AndroidDriver driver;
-        public AppiumDriverLocalService service;
-        public FormPage formPage;
+public class BaseTest {
 
-        @BeforeClass
-        public void ConfigureAppium() throws IOException {
+    public AndroidDriver driver;
+    public FormPage formPage;
 
-            Properties prop = new Properties();
+    @BeforeClass
+    public void ConfigureAppium() throws Exception {
 
-            InputStream input = getClass()
-                    .getClassLoader()
-                    .getResourceAsStream("data.properties");
+        Properties prop = new Properties();
 
-            if (input == null) {
-                throw new FileNotFoundException("data.properties not found in classpath");
-            }
+        InputStream input = getClass()
+                .getClassLoader()
+                .getResourceAsStream("data.properties");
 
-            prop.load(input);
+        if (input == null) {
+            throw new FileNotFoundException(
+                    "data.properties not found in classpath"
+            );
+        }
 
+        prop.load(input);
 
-            String ipAddress =prop.getProperty("ipAddress");
-            String port =prop.getProperty("port");
-            //service =startAppiumServer(ipAddress,Integer.parseInt(port));
+        String ipAddress = prop.getProperty("ipAddress");
+        String port = prop.getProperty("port");
 
-        //code to start server
-            service = AppiumDriverLocalService.buildService(
-                    new AppiumServiceBuilder()
-                    .withAppiumJS(new File("/usr/local/lib/node_modules/appium/build/lib/main.js"))
-                    .withIPAddress(ipAddress)
-                    .usingPort(Integer.parseInt(port))
-        );
-        service.start();
+        UiAutomator2Options options = new UiAutomator2Options();
 
-
-        UiAutomator2Options options =new UiAutomator2Options();
         options.setDeviceName("MobileTestSurekha");
-        options.setChromedriverExecutable("C://Users//msurekha//chromedriver//chromedriver124//chromedriver-win64/chromedriver.exe");
-        //options.setCapability("appium:showChromedriverLog", true);
-        options.setApp("C://Users//msurekha//IdeaProjects//MobilePOMFramework1//src//main//resources//General-Store.apk");
-        //options.setApp("C://Users//msurekha//IdeaProjects//MobileTest//src//test//java//resources//ApiDemos-debug.apk");
-        //options.setCapability("appium:extractChromeAndroidPackageFromContextName", true);
-         driver =new AndroidDriver( new URL("http://127.0.0.1:4723"),options);
-         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-         formPage =new FormPage(driver);
 
+        options.setApp(
+                System.getProperty("user.dir")
+                        + "/src/main/resources/General-Store.apk"
+        );
 
+        URL appiumServerUrl =
+                new URL("http://" + ipAddress + ":" + port);
+
+        driver = new AndroidDriver(
+                appiumServerUrl,
+                options
+        );
+
+        driver.manage()
+                .timeouts()
+                .implicitlyWait(Duration.ofSeconds(10));
+
+        formPage = new FormPage(driver);
     }
 
     @AfterClass
-    public void tearDown(){
-        if(driver!=null){
-        driver.quit();}
-        service.stop();
-    }
+    public void tearDown() {
 
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
